d609ded63d06dd2e93eef97eacd914a5948091d0
Jason Carter <Jcarter91@gmail.com>
2018-10-18 08:26:04 -0500

    contact update

--- togs.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-        var inputValue = $(this).attr("value");
-        var contact2box = $("." + inputValue);
-        $(".contact2").not(contact2box).hide();
-        $(contact2box).show();
-    });
-});
